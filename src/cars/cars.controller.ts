76b73d19f3497625ea934af91f9ac09a1b082f29
Smilling Battaglini <vanessabattaglini@gmail.com>
2024-06-27 18:58:28 -0400

    :sparkles: Creación de servicio, inyección de dependencias, peticiones get y post

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Body, Patch, Delete } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
+    
 export class CarsController {
+
+    constructor(
+        private readonly carService: CarsService
+    ){}
     @Get()
     getAllCars() {
-        return ['Toyota', 'Honda', 'Jeep']
+        return this.carService.findAll();
+    }
+    @Get(':id') 
+    getById(@Param('id', ParseIntPipe) id: number) {
+        console.log({id})
+        return this.carService.findOneById(id);
+    };
+    
+    @Post()
+    createCar( @Body() body:any) {
+        return body;
     }
 
-}
+    @Patch(':id')
+    updateCar(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: any) {
+        return body;
+    };
+
+    @Delete(':id')
+    deleteCar(@Param('id', ParseIntPipe) id: number) {
+        return {
+            method: "Delete",
+            id
+        };
+    }
+
+    }
+
